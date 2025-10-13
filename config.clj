027ceac91bc9cac779eb203bf1c6d4880aff7128
Sonny To <son.c.to@gmail.com>
2025-10-13 23:20:53 +0800

    / -> overview.chp

--- config.clj
+++ config.clj
@@ -1,5 +1,5 @@
 {:port 3002
  :chp-dir "public/chp"
  :public-dir "public"
- :bidi-routes ["/" [["" (fn [req] (stigmergy.chp/hiccup-page-handler (assoc req :uri "/index.chp")))]
+ :bidi-routes ["/" [["" (fn [req] (stigmergy.chp/hiccup-page-handler (assoc req :uri "/overview.chp")))]
                     [#".*\.chp"  stigmergy.chp/hiccup-page-handler]]]}
