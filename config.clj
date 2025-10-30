6dd1357c19b63b68bf1aa8ad1ad4e0ecae3a8d5d
Sonny To <son.c.to@gmail.com>
2025-10-31 02:04:41 +0700

    overview.chp -> datomworld.chp

--- config.clj
+++ config.clj
@@ -1,5 +1,5 @@
 {:port 3002
  :chp-dir "public/chp"
  :public-dir "public"
- :bidi-routes ["/" [["" (fn [req] (stigmergy.chp/hiccup-page-handler (assoc req :uri "/overview.chp")))]
+ :bidi-routes ["/" [["" (fn [req] (stigmergy.chp/hiccup-page-handler (assoc req :uri "/datomworld.chp")))]
                     [#".*\.chp"  stigmergy.chp/hiccup-page-handler]]]}
