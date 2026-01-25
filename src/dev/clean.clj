99071dab9ba4ae3579db85a206ed956f2b027775
sto <son.c.to@gmail.com>
2026-01-24 23:30:23 -0500

    clean compiled assets

---
+++ src/dev/clean.clj
@@ -0,0 +1,13 @@
+(ns clean
+  (:require [clojure.java.shell :as sh]
+            [clojure.java.io :as io]))
+
+(defn clean [args]
+  (let [paths [".cpcache" "target" ".shadow-cljs" "public/js" "lib/cljd-out" ".clojuredart"]
+        existing-paths (filter #(.exists (io/file %)) paths)]
+    (if (seq existing-paths)
+      (do
+        (println "Cleaning:" (clojure.string/join ", " existing-paths))
+        (apply sh/sh "rm" "-rf" existing-paths)
+        (println "Clean complete."))
+      (println "Nothing to clean."))))
