eb429b13834fe5d0249d040b4f911c4d0913000c
sto <son.c.to@gmail.com>
2026-02-02 05:48:59 -0500

    chore(fmt): add unified :fix alias for cljstyle and zprint

--- src/dev/tasks.clj
+++ src/dev/tasks.clj
@@ -3,16 +3,16 @@
             [clojure.string :as str]))
 
 
-(defn cljstyle-fix
-  [_]
+(defn- run-cljstyle
+  []
   (let [{:keys [exit out err]} (sh/sh "mise" "exec" "--" "cljstyle" "fix")]
     (when-not (str/blank? out) (println out))
     (when-not (str/blank? err) (binding [*out* *err*] (println err)))
-    (System/exit exit)))
+    exit))
 
 
-(defn zprint-fix
-  [_]
+(defn- run-zprint
+  []
   (let [files (-> (sh/sh "find" "src" "test" "-name" "*.clj*")
                   :out
                   (str/split #"\s+"))
@@ -22,5 +22,18 @@
               (apply sh/sh "mise" "exec" "--" "zprint" "-w" valid-files)]
         (when-not (str/blank? out) (println out))
         (when-not (str/blank? err) (binding [*out* *err*] (println err)))
-        (System/exit exit))
-      (println "No files found to format."))))
+        exit)
+      (do (println "No files found to format.") 0))))
+
+
+(defn cljstyle-fix [_] (System/exit (run-cljstyle)))
+
+
+(defn zprint-fix [_] (System/exit (run-zprint)))
+
+
+(defn fix-all
+  [_]
+  (let [exit1 (run-cljstyle)
+        exit2 (run-zprint)]
+    (System/exit (if (or (not= 0 exit1) (not= 0 exit2)) 1 0))))
